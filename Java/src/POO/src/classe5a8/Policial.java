676a5f91d54d028b4970ecbe049651fca7f8ef86
Henryzin-11 <Luanhenriquedasilvapam@gmail.com>
2026-09-17 16:16:58 -0300

    Adiciona Classes 5 A 8

---
+++ Java/src/POO/src/classe5a8/Policial.java
@@ -0,0 +1,17 @@
+package classe5a8;
+
+public class Policial {
+    int nivel_condicionamento_fisico;
+    String nocao_tatica;
+    String Controle_equipamento_seguranca;
+    String Porte_arma;
+
+    public void algemarSuspeito(){
+        System.out.println("Voce esta preso..!");
+
+    }
+    public void atenderChamado(){
+            System.out.println("Estou a caminho.");
+
+}
+}
