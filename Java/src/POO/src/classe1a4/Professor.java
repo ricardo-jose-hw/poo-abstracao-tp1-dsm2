7fdf0479458b68d8274542eac3814acde97e1f89
lucassoarescavalcanti-commits <lucassoarescavalcanti@gmail.com>
2026-09-16 21:26:12 -0300

    Adiciona atributos e métodos na classe Professor

--- Java/src/POO/src/classe1a4/Professor.java
+++ Java/src/POO/src/classe1a4/Professor.java
@@ -1,5 +1,16 @@
 package classe1a4;
 
 public class Professor {
+    public String licenciatura;
+    public boolean didatica;
+    public String nome;
+    public int idade;
+}
 
+public void ensinar(){
+    System.out.println("Eu dou aula de " + this.licenciatura+ " para os alunos.");
+}
+
+public void aplicarProva(){
+    System.out.println("Vou aplicar prova em breve, estudem!");
 }
