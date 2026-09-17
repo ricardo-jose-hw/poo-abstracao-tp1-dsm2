289c68cbf48ba009e60c6cc9a64e7542c4ce3094
guigocampos <guicamposreis08@gmail.com>
2026-09-17 15:12:16 -0300

    Implementa codigo advogado

---
+++ Java/src/POO/src/classe9a12/Advogado.java
@@ -0,0 +1,24 @@
+package classe9a12;
+
+public class Advogado {
+
+    String nome;
+    int idade;
+    String oab;
+    String areaAtuacao;
+
+    public Advogado(String nome, int idade, String oab, String areaAtuacao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.oab = oab;
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public void defenderCliente() {
+        System.out.println(nome + " está defendendo um cliente.");
+    }
+
+    public void elaborarProcesso() {
+        System.out.println(nome + " está elaborando um processo.");
+    }
+}
