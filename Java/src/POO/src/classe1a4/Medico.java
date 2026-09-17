c1b136587ae7c0dc3ffb2e62bc248c1e47eef95f
lucassoarescavalcanti-commits <lucassoarescavalcanti@gmail.com>
2026-09-16 22:17:01 -0300

    Adiciona classe Medico

---
+++ Java/src/POO/src/classe1a4/Medico.java
@@ -0,0 +1,16 @@
+package classe1a4;
+
+public class Medico {
+    public String especialidade;
+    public String nome;
+    public String hospital;
+    public int anosExpericia;
+
+    public void atenderPaciente(){
+        System.out.println("Olá, o que você está sentindo?");
+    }
+
+    public void medicar(){
+        System.out.println("Vou prescrever um Benzetacil para você.");
+    }
+}
